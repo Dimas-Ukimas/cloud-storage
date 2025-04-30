0d12a6031512d01142130f580d68b7e550a69356
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-04-30 14:13:55 +0300

    Initial commit

---
+++ src/main/java/com/dimasukimas/cloud/CloudApplication.java
@@ -0,0 +1,10 @@
+package com.dimasukimas.cloud;
+
+@SpringBootApplication
+public class CloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+
+}
