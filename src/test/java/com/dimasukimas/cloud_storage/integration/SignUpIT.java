83e5365ce05437ed972e5aca63f1e96ecd8d2b6a
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-06-07 21:15:13 +0300

    test: refactor SignUp integration test

--- src/test/java/com/dimasukimas/cloud_storage/integration/SignUpIT.java
+++ src/test/java/com/dimasukimas/cloud_storage/integration/SignUpIT.java
@@ -1,20 +1,27 @@
 package com.dimasukimas.cloud_storage.integration;
 
 
-import jakarta.servlet.ServletContext;
-import org.assertj.core.api.Assertions;
+import com.dimasukimas.cloud_storage.dto.AuthResponseDto;
+import com.dimasukimas.cloud_storage.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.core.env.Environment;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -24,38 +31,24 @@ public class SignUpIT {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
-
-    @Container
-    @ServiceConnection
-    static GenericContainer<?> genericContainer = new GenericContainer<>("redis:7-alpine")
-            .withExposedPorts(6379);
-
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
     @Autowired
-    private Environment environment;
+    private UserRepository userRepository;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @Container
+    @ServiceConnection
+    static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
+            .withExposedPorts(6379)
+            .waitingFor(Wait.forListeningPort());
 
     @Test
-    void printContextPath() {
-        String contextPath = environment.getProperty("server.servlet.context-path");
-        System.out.println("Loaded context path = " + contextPath);
-    }
-    @Test
-    void checkContextPathFromServletContext(@Autowired ServletContext servletContext) {
-        System.out.println("Servlet context path: " + servletContext.getContextPath());
-    }
-
-    @Test
-    void printBaseUrl() {
-        String baseUrl = testRestTemplate.getRestTemplate().getUriTemplateHandler().expand("/").toString();
-        System.out.println("Base URL = " + baseUrl);
-    }
-
-
-    @Test
-    public void givenValidRegistrationData_whenSignUp_thenUserIsCreated() throws Exception {
+    public void givenValidRegistrationData_whenSignUp_thenUserAndSessionIsCreatedWithAppropriateHeadersAndStatusCode() throws Exception {
 
         String json = """
                 {
@@ -63,12 +56,26 @@ public class SignUpIT {
                     "password": "secret"
                 }
                 """;
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> request = new  HttpEntity<>(json, headers);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
 
         ResponseEntity<String> response = testRestTemplate.postForEntity("/auth/sign-up", request, String.class);
 
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        AuthResponseDto responseDto = objectMapper.readValue(response.getBody(), AuthResponseDto.class);
+        assertThat(responseDto.username()).isEqualTo("user");
+
+        assertThat(userRepository.findByUsername("user")).isPresent();
+
+        List<String> setCookies = response.getHeaders().get(HttpHeaders.SET_COOKIE);
+        assertThat(setCookies).anyMatch(cookie -> cookie.startsWith("SESSION"));
+
+        Set<String> keys = redisTemplate.keys("spring:session:sessions:*");
+        assertThat(keys).isNotEmpty();
     }
 }
