4ee58b13b890ab4d3cbc5500b06ea2ac3e8e3acd
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-06-25 19:13:18 +0300

    feat: add exception handling for auth

---
+++ src/main/java/com/dimasukimas/cloud_storage/exception/UnauthorizedUserSignOutException.java
@@ -0,0 +1,9 @@
+package com.dimasukimas.cloud_storage.exception;
+
+public class UnauthorizedUserSignOutException extends RuntimeException {
+
+    public UnauthorizedUserSignOutException(String message){
+        super(message);
+    }
+
+}
