e2e8a99b3013a011b9916633920ee9aafef1a22c
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-07-24 21:17:01 +0300

    test: add WithCustomUser annotation and WithCustomSecurityContextFactory

---
+++ src/test/java/com/dimasukimas/cloud_storage/config/WithCustomUser.java
@@ -0,0 +1,22 @@
+package com.dimasukimas.cloud_storage.config;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@WithSecurityContext(factory = WithCustomSecurityContextFactory.class)
+public @interface WithCustomUser {
+
+    long id() default 1L;
+
+    String username() default "testUser";
+
+    String password() default "password";
+
+    String role() default "ROLE_USER";
+}
