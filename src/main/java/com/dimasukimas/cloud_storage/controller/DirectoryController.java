9bd136621481d76e2259805e3bb0d27e2cf35a8a
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-07-24 21:15:46 +0300

    feat: add DirectoryController class with post mapping for directory creation

--- src/main/java/com/dimasukimas/cloud_storage/controller/DirectoryController.java
+++ src/main/java/com/dimasukimas/cloud_storage/controller/DirectoryController.java
@@ -1,4 +1,29 @@
 package com.dimasukimas.cloud_storage.controller;
 
+import com.dimasukimas.cloud_storage.dto.CustomUserDetails;
+import com.dimasukimas.cloud_storage.dto.DirectoryInfoDto;
+import com.dimasukimas.cloud_storage.service.ResourceManagerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/directory")
+@RequiredArgsConstructor
 public class DirectoryController {
+
+    private final ResourceManagerService resourceManagerService;
+
+    @PostMapping
+    public ResponseEntity<DirectoryInfoDto> createDirectory(@RequestParam String path,
+                                                            @AuthenticationPrincipal CustomUserDetails user) {
+        DirectoryInfoDto directoryInfo = resourceManagerService.createDirectory(user.id(), path);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(directoryInfo);
+    }
+
 }
