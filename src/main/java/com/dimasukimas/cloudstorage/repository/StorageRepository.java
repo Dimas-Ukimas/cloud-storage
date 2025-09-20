25e015d2958c1f2160f7adece55405495d17c0ea
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-09-20 19:00:01 +0300

    refactor: add return type in StorageRepository interface for createDirectory()

--- src/main/java/com/dimasukimas/cloudstorage/repository/StorageRepository.java
+++ src/main/java/com/dimasukimas/cloudstorage/repository/StorageRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StorageRepository {
-    void createDirectory(String path);
+    ObjectInfo createDirectory(String path);
 
     boolean isObjectExists(String path);
 
