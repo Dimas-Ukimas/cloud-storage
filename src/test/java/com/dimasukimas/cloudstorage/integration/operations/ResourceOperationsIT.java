b3e5dffb6f3182623305a6395d843ece6f293e4d
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-09-20 19:27:27 +0300

    test: group tests in ResourceOperationsIT via @Nested, add @DisplayName for each test and add new tests

--- src/test/java/com/dimasukimas/cloudstorage/integration/operations/ResourceOperationsIT.java
+++ src/test/java/com/dimasukimas/cloudstorage/integration/operations/ResourceOperationsIT.java
@@ -12,9 +12,7 @@ import com.dimasukimas.cloudstorage.helper.RequestTestHelper;
 import com.dimasukimas.cloudstorage.helper.UserTestDataHelper;
 import com.dimasukimas.cloudstorage.util.assertion.HttpAssert;
 import com.dimasukimas.cloudstorage.util.assertion.MinioAssert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -30,7 +28,6 @@ import java.util.List;
 @IntegrationTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = "app.security.enabled=false")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import(SecurityTestConfig.class)
 @ContextConfiguration(initializers = {
         PostgresContainerInitializer.class,
@@ -53,7 +50,7 @@ public class ResourceOperationsIT {
 
     private static final String USERNAME = "testUser";
     private static final String PASSWORD = "secret";
-    private static String userRootDirectory;
+    private String userRootDirectory;
 
     @BeforeEach
     void setUp() {
@@ -63,61 +60,155 @@ public class ResourceOperationsIT {
         userRootDirectory = minioHelper.createUserRootDirectory(userId);
     }
 
-    @Test
-    void givenValidPath_whenCreateDirectory_thenCreationSuccessful() {
-        ResponseEntity<ResourceInfoDto> response = testRestTemplate.postForEntity(
-                "/directory?path=folder1/",
-                null,
-                ResourceInfoDto.class
-        );
+    @Nested
+    @DisplayName("POST /directory")
+    class CreateDirectory {
 
-        HttpAssert.create(response)
-                .assertJsonContentType()
-                .assertStatus(HttpStatus.CREATED)
-                .assertBodyContainsResourceName("folder1/");
+        @Test
+        @DisplayName("201 when create a new directory with valid path")
+        void givenValidPath_whenCreateDirectory_thenCreationSuccessful() {
+            ResponseEntity<ResourceInfoDto> response = testRestTemplate.postForEntity(
+                    "/directory?path=folder1/",
+                    null,
+                    ResourceInfoDto.class
+            );
 
-        MinioAssert.create(minioHelper)
-                .assertDirectoryExist(userRootDirectory + "folder1/");
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.CREATED)
+                    .assertBodyContainsResourceName("folder1/");
+
+            MinioAssert.create(minioHelper)
+                    .assertDirectoryExist(userRootDirectory + "folder1/");
+        }
+
+        @Test
+        @DisplayName("409 when creating an already existent directory")
+        void givenValidPath_whenCreateAlreadyExistentDirectory_thenReturnConflict() {
+            testRestTemplate.postForEntity(
+                    "/directory?path=folder1/",
+                    null,
+                    ResourceInfoDto.class
+            );
+
+            ResponseEntity<ErrorResponse> response = testRestTemplate.postForEntity(
+                    "/directory?path=folder1/",
+                    null,
+                    ErrorResponse.class
+            );
+
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.CONFLICT)
+                    .assertBodyContainsMessage("Resource is already exists");
+
+            MinioAssert.create(minioHelper)
+                    .assertDirectoryNotExists(userRootDirectory + "folder1/");
+        }
+
+        @Test
+        @DisplayName("404 when parent directory does not exists")
+        void givenNotExistentPath_whenCreateDirectory_thenReturnNotFound() {
+            ResponseEntity<ErrorResponse> response = testRestTemplate.postForEntity(
+                    "/directory?path=folder404/folder1/",
+                    null,
+                    ErrorResponse.class
+            );
+
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.NOT_FOUND)
+                    .assertBodyContainsMessage("Parent directory does not exists");
+
+            MinioAssert.create(minioHelper)
+                    .assertDirectoryNotExists(userRootDirectory + "folder404/folder1/");
+        }
+
+
     }
 
-    @Test
-    void givenValidPath_whenGetEmptyDirectoryContentInfo_thenReturnInfo() {
-        testRestTemplate.postForEntity(
-                "/directory?path=folder1/",
-                null,
-                ResourceInfoDto.class
-        );
+    @Nested
+    @DisplayName("GET /directory")
+    class ListDirectory {
 
-        ResponseEntity<List<ResourceInfoDto>> response = testRestTemplate.exchange(
-                "/directory?path=folder1/",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
+        @BeforeEach
+        void setUp() {
+            minioHelper.clearBucket();
+            minioHelper.createDirectory(userRootDirectory + "folder1/");
+        }
 
-        HttpAssert.create(response)
-                .assertJsonContentType()
-                .assertStatus(HttpStatus.OK)
-                .assertEmptyBody();
+        @Test
+        @DisplayName("200 with empty directory content info when get empty directory")
+        void givenExistentPath_whenGetEmptyDirectoryContentInfo_thenReturnInfo() {
+            ResponseEntity<List<ResourceInfoDto>> response = testRestTemplate.exchange(
+                    "/directory?path=folder1/",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<>() {
+                    }
+            );
+
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.OK)
+                    .assertEmptyBody();
+        }
+
+        @Test
+        @DisplayName("404 when get non-existent directory")
+        void givenNotExistentPath_whenGetDirectoryContentInfo_thenNotFound() {
+            ResponseEntity<ErrorResponse> response = testRestTemplate.exchange(
+                    "/directory?path=folder404/",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<>() {
+                    }
+            );
+
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.NOT_FOUND)
+                    .assertBodyContainsMessage("Resource does not exists");
+        }
     }
 
-    @Test
-    void givenNotExistentPath_whenGetDirectoryContentInfo_thenNotFound() {
-        ResponseEntity<ErrorResponse> response = testRestTemplate.exchange(
-                "/directory?path=non-existent-folder/",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
+    @Nested
+    @DisplayName("GET /resource")
+    class GetResource {
 
-        HttpAssert.create(response)
-                .assertJsonContentType()
-                .assertStatus(HttpStatus.NOT_FOUND)
-                .assertBodyContainsMessage("Resource does not exists");
+        @BeforeEach
+        void setUp() {
+            minioHelper.clearBucket();
+            minioHelper.createDirectory(userRootDirectory + "folder1/");
+        }
+
+        @Test
+        @DisplayName("200 with resource info when get existent resource")
+        void givenExistentResourcePath_whenGetResourceInfo_thenReturnOk() {
+            ResponseEntity<ResourceInfoDto> response = testRestTemplate.getForEntity(
+                    "/resource?path=folder1/",
+                    ResourceInfoDto.class
+            );
+
+            HttpAssert.create(response)
+                    .assertJsonContentType()
+                    .assertStatus(HttpStatus.OK)
+                    .assertBodyContainsResourceName("folder1/");
+        }
+
+
+        @Test
+        @DisplayName("404 when get non-existent resource")
+        void givenNotExistentResourcePath_whenGetResourceInfo_thenReturnNotFound() {
+            ResponseEntity<ErrorResponse> response = testRestTemplate.getForEntity(
+                    "/resource?path=folder404/",
+                    ErrorResponse.class
+            );
+
+            HttpAssert.create(response)
+                    .assertStatus(HttpStatus.NOT_FOUND)
+                    .assertJsonContentType()
+                    .assertBodyContainsMessage("Resource does not exists");
+        }
     }
-
-
-
 }
