ee109e6a15fda5d9c5a6f6e893c5314872c3fa18
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-09-30 20:20:38 +0300

    test: add TestUtils class

---
+++ src/test/java/com/dimasukimas/cloudstorage/util/TestUtils.java
@@ -0,0 +1,30 @@
+package com.dimasukimas.cloudstorage.util;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+public class TestUtils {
+
+    public static HttpEntity<MultiValueMap<String, Object>> createRequestWithTestFile(String fileName, String content) {
+        byte[] bytes = content.getBytes();
+        ByteArrayResource file = new ByteArrayResource(bytes) {
+            @Override
+            public String getFilename() {
+                return fileName;
+            }
+        };
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file);
+
+        return new HttpEntity<>(body, headers);
+    }
+
+}
